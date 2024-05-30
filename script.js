b5dc3e5449d6601e2b5934650820b392f562ec15
Adhulvas <adhulvas2000@gmail.com>
2024-05-30 18:57:34 +0530

    Third

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ let length=12
 let width=8
 let area=length * width
 console.log('Area of the rectangle is ' + area);
+
+// Create a program that asks the user for their age and determines if they are eligible to vote (considering a voting age of 18).
+
+const age = 18
+let myAge = 17
+
+if (myAge >= age) {
+    console.log('You are eligible to vote');
+}else {
+    console.log('You are not eligible to vote');
+}
