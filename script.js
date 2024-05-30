f48003ed86fcf4b5afcd16e2eabf7598879b7a1a
Adhulvas <adhulvas2000@gmail.com>
2024-05-30 13:24:36 +0530

    second question

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ let num1=230
 let num2=270
 let sum=num1+num2
 console.log('Result is ' + sum);
+
+// Implement a program that calculates the area of a rectangle using given length and width.
+
+let length=12
+let width=8
+let area=length * width
+console.log('Area of the rectangle is ' + area);
